bb6e87b48aeb0f81dbf60f2d6ce5e9898b0c841b
Wbofeng <1464226975@qq.com>
2017-12-15 23:19:31 +0800

    Operators

--- Rxjs/src/components/HelloWorld.vue
+++ Rxjs/src/components/HelloWorld.vue
@@ -150,7 +150,7 @@ export default {
           next: v => console.log('observerB: ' + v), // eslint-disable-line prefer-template
         });
       }, 1000); */
-      const subject = new Rx.AsyncSubject();
+      /*  const subject = new Rx.AsyncSubject();
 
       subject.subscribe({
         next: v => console.log('observerA: ' + v), // eslint-disable-line prefer-template
@@ -166,7 +166,21 @@ export default {
       });
 
       subject.next(5);
-      subject.complete();
+      subject.complete(); */
+      function multiplyByTen(input) {
+        const output = Rx.Observable.create(function subscribe(observer) {
+          input.subscribe({
+            next: v => observer.next(10 * v),
+            error: err => observer.error(err),
+            complete: () => observer.complete(),
+          });
+        });
+        return output;
+      }
+
+      const input = Rx.Observable.from([1, 2, 3, 4]);
+      const output = multiplyByTen(input);
+      output.subscribe(x => console.log(x));
     },
   },
 };
